5c04fe6222c7a450da893f819d6c4e605a8f5e5c
Benjamin Orr <benorr3@gmail.com>
2026-01-23 16:45:40 -0500

    work on feature matching

--- Group11_p1/Phase1/Code/Wrapper.py
+++ Group11_p1/Phase1/Code/Wrapper.py
@@ -51,11 +51,12 @@ def ANMS(C_img, N_best):
 
 def encode_feature_points(image, corners):
     descriptors = []
+    valid_corners = []
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     H, W = gray.shape
     PATCH_RADIUS = 20
     for y, x in corners:
-        # extract 40x40 patch around corner
+        # extract 41x41 patch around corner
         if (
             x - PATCH_RADIUS < 0
             or x + PATCH_RADIUS >= W
@@ -64,16 +65,67 @@ def encode_feature_points(image, corners):
         ):
             continue
         patch = gray[
-            y - PATCH_RADIUS : y + PATCH_RADIUS, x - PATCH_RADIUS : x + PATCH_RADIUS
+            y - PATCH_RADIUS : y + PATCH_RADIUS + 1,
+            x - PATCH_RADIUS : x + PATCH_RADIUS + 1,
         ]
-        patch = cv2.GaussianBlur(patch, (5, 5), sigmaX=0)
+        patch = cv2.GaussianBlur(patch, (0, 0), sigmaX=2)
         # downsample, normalize, and flatten
-        small_patch = cv2.resize(patch, (8, 8), interpolation=cv2.INTER_AREA)
+        small_patch = cv2.resize(patch, (8, 8), interpolation=cv2.INTER_AREA).astype(
+            np.float32
+        )
         std = max(np.std(small_patch), 1e-5)
         norm_patch = (small_patch - np.mean(small_patch)) / std
         descriptor = norm_patch.flatten()
         descriptors.append(descriptor)
-    return np.array(descriptors)
+        valid_corners.append((y, x))
+    # encoded feature descriptors, corresponding corner locations
+    return np.array(descriptors), np.array(valid_corners)
+
+
+def match_features(desc1, desc2, ratio_thresh=0.7):
+    matches = []
+    for i, d1 in enumerate(desc1):
+        # compute sum of square difference with all descriptors in desc2
+        distances = np.sum((desc2 - d1) ** 2, axis=1)
+        # find the two closest matches
+        best_match_idx = np.argmin(distances)
+        dist_best = distances[best_match_idx]
+        distances[best_match_idx] = (
+            np.inf
+        )  # exclude the nearest neighbor to find the second nearest
+        dist_second_best = distances[np.argmin(distances)]
+        # apply ratio test
+        if dist_best / dist_second_best < ratio_thresh:
+            matches.append((i, best_match_idx))
+    return matches  # list of (index in desc1, index in desc2)
+
+
+def _corners_to_keypoints(corners):
+    # convert corner coordinates (y,x) to cv2.KeyPoint objects
+    keypoints = []
+    for y, x in corners:
+        kp = cv2.KeyPoint(x=float(x), y=float(y), size=5)
+        keypoints.append(kp)
+    return keypoints
+
+
+def _matches_to_dmatches(matches):
+    # convert (i,j) tuples to cv2.DMatch objects
+    dmatches = []
+    for i, j in matches:
+        dm = cv2.DMatch(_queryIdx=i, _trainIdx=j, _distance=0)
+        dmatches.append(dm)
+    return dmatches
+
+
+def draw_matches(image1, image2, matches, keypoints):
+    kp1 = _corners_to_keypoints(keypoints[0])
+    kp2 = _corners_to_keypoints(keypoints[1])
+    dmatches = _matches_to_dmatches(matches)
+    img_matches = cv2.drawMatches(image1, kp1, image2, kp2, dmatches, None)
+    cv2.imshow("Feature Matches", img_matches)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 def main():
@@ -102,18 +154,18 @@ def main():
             if img is not None:
                 images.append(img)
     print(f"Read {len(images)} images from {input_dir}")
-    image1 = images[0]
+
     """
     Corner Detection
     Save Corner detection output as corners.png
     """
-    raw_corners = locate_corners(image1)
+    raw_corners = [locate_corners(image) for image in images]
 
     """
     Perform ANMS: Adaptive Non-Maximal Suppression
     Save ANMS output as anms.png
     """
-    anms_corners = ANMS(raw_corners, 100)
+    anms_corners = [ANMS(corners, 100) for corners in raw_corners]
     # for y, x in anms_corners:
     #     cv2.circle(image1, (x, y), 3, (0, 0, 255), -1)
     # cv2.imshow("ANMS Corners", image1)
@@ -124,12 +176,20 @@ def main():
     Feature Descriptors
     Save Feature Descriptor output as FD.png
     """
-    fd = encode_feature_points(image1, anms_corners)
+    fd, keypoints = zip(
+        *[
+            encode_feature_points(image, corners)
+            for image, corners in zip(images, anms_corners)
+        ]
+    )
 
     """
     Feature Matching
     Save Feature Matching output as matching.png
     """
+    matches = match_features(fd[0], fd[1])
+    # perform conversions for visualization
+    draw_matches(images[0], images[1], matches, keypoints)
 
     """
     Refine: RANSAC, Estimate Homography
