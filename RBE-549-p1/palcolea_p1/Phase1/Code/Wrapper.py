c35abe0b199b8e38d7fe7e3a7cd220600e0f21b4
Pau Alcolea Vila <paualcoleavila@Paus-MacBook-Air.local>
2026-01-23 00:26:40 -0500

    code for getting the corners

--- RBE-549-p1/palcolea_p1/Phase1/Code/Wrapper.py
+++ RBE-549-p1/palcolea_p1/Phase1/Code/Wrapper.py
@@ -16,20 +16,58 @@ Worcester Polytechnic Institute
 import numpy as np
 import cv2
 import os
+import copy
 
-def read_set(image_set_path: str) -> list[np.array]:
+"""
+This function takes a path where a set of images to be made panorama is
+It transforms the images into gray and opens them
+@returns images wich is a list of np.arrays with the images in RGB
+@returns images_gray which is the same except in gray scale, which is needed for the harris detector
+"""
+def read_set(image_set_path: str) -> tuple[list[np.array], list[np.array]]:
 	images = []
+	images_gray = []
 	
 	#iterate through all of the files in the Image_set folder
 	for file in os.listdir(image_set_path):
 		if file.lower().endswith(".jpg"):
 			# join the two names to make overall path of the image to then open with imread
+			# image has to be gray and a float for the harris detector
 			img_path = os.path.join(image_set_path, file)
 			img = cv2.imread(img_path)
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-			images.append(img)
+			img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+			img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			img_gray = np.float32(img_gray)
+			images.append(img_rgb)
+			images_gray.append(img_gray)
 
-	return images
+	return images, images_gray
+
+
+"""
+this functin finds the corners in an image with the harris filter
+used the opencv2 documentation: https://docs.opencv.org/4.x/dc/d0d/tutorial_py_features_harris.html
+@params, two lists of images, in color and in grayscale
+@returns one list of images with the corners
+"""
+def corner_detection(images_color: list[np.array], images_gray: list[np.array]) -> list[np.array]:
+	# iterate through all of the images in the set
+	for index, img_gray in enumerate(images_gray):
+		
+		# (H,W) array with all of the scores
+		change_score = cv2.cornerHarris(img_gray, 2, 3, 0.04)
+
+		# make a boolean mask if any score is more than 1% of the maximum corner score
+		# wherever the mask is true (corner), change the pixel to 0,0,255 (aka red)
+		images_color[index][change_score>0.01*change_score.max()]=[0,0,255]
+		images_cornered = copy.deepcopy(images_color)
+
+		# The following code is for visualization
+		# cv2.imshow('dst',images_cornered[index])
+		# if cv2.waitKey(0) & 0xff == 27:
+		# 	cv2.destroyAllWindows()
+	
+	return images_cornered
 
 def main():
     # Add any Command Line arguments here
@@ -42,14 +80,13 @@ def main():
 	"""
     Read a set of images for Panorama stitching
     """
-	images = read_set("RBE-549-p1/palcolea_p1/Phase1/Data/Train/Set1/")
-	print(images)
-
+	images, images_gray = read_set("RBE-549-p1/palcolea_p1/Phase1/Data/Train/Set1/")
 
 	"""
 	Corner Detection
 	Save Corner detection output as corners.png
 	"""
+	corners = corner_detection(images, images_gray)
 
 	"""
 	Perform ANMS: Adaptive Non-Maximal Suppression
