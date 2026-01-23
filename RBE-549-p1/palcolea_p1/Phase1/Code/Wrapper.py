3c633137f3085dc2f9fef5c2b55e4ee2d6e87afd
Pau Alcolea Vila <paualcoleavila@Paus-MacBook-Air.local>
2026-01-22 23:55:39 -0500

    reading a set from data directory

--- RBE-549-p1/palcolea_p1/Phase1/Code/Wrapper.py
+++ RBE-549-p1/palcolea_p1/Phase1/Code/Wrapper.py
@@ -15,9 +15,21 @@ Worcester Polytechnic Institute
 # Code starts here:
 import numpy as np
 import cv2
+import os
 
-# Add any python libraries here
+def read_set(image_set_path: str) -> list[np.array]:
+	images = []
+	
+	#iterate through all of the files in the Image_set folder
+	for file in os.listdir(image_set_path):
+		if file.lower().endswith(".jpg"):
+			# join the two names to make overall path of the image to then open with imread
+			img_path = os.path.join(image_set_path, file)
+			img = cv2.imread(img_path)
+			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+			images.append(img)
 
+	return images
 
 def main():
     # Add any Command Line arguments here
@@ -30,6 +42,9 @@ def main():
 	"""
     Read a set of images for Panorama stitching
     """
+	images = read_set("RBE-549-p1/palcolea_p1/Phase1/Data/Train/Set1/")
+	print(images)
+
 
 	"""
 	Corner Detection
@@ -60,8 +75,6 @@ def main():
 	Image Warping + Blending
 	Save Panorama output as mypano.png
 	"""
-
-	print(np.pi)
 	return
 
 
