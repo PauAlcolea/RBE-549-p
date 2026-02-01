54866406031804598c45009f78ee4f9430ea0c22
Benjamin Orr <benorr3@gmail.com>
2026-02-01 14:51:05 -0500

    work on Network code

--- Group11_p1/Phase2/Code/Network/Network.py
+++ Group11_p1/Phase2/Code/Network/Network.py
@@ -16,29 +16,17 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 import kornia  # You can use this to get the transform and warp in this project
+import pytorch_lightning as pl
 
 # Don't generate pyc codes
 sys.dont_write_bytecode = True
 
 
-def LossFn(delta, img_a, patch_b, corners):
-    ###############################################
-    # Fill your loss function of choice here!
-    ###############################################
-
-    ###############################################
-    # You can use kornia to get the transform and warp in this project
-    # Bonus if you implement it yourself
-    ###############################################
-    loss = ...
-    return loss
-
-
-class HomographyModel(pl.LightningModule):
+class SupervisedHomographyModel(pl.LightningModule):
     def __init__(self, hparams):
-        super(HomographyModel, self).__init__()
+        super(SupervisedHomographyModel, self).__init__()
         self.hparams = hparams
-        self.model = Net()
+        self.model = SupervisedNet()
 
     def forward(self, a, b):
         return self.model(a, b)
@@ -46,14 +34,14 @@ class HomographyModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         img_a, patch_a, patch_b, corners, gt = batch
         delta = self.model(patch_a, patch_b)
-        loss = LossFn(delta, img_a, patch_b, corners)
+        loss = F.smooth_l1_loss(delta, gt)
         logs = {"loss": loss}
         return {"loss": loss, "log": logs}
 
     def validation_step(self, batch, batch_idx):
         img_a, patch_a, patch_b, corners, gt = batch
         delta = self.model(patch_a, patch_b)
-        loss = LossFn(delta, img_a, patch_b, corners)
+        loss = F.smooth_l1_loss(delta, gt)
         return {"val_loss": loss}
 
     def validation_epoch_end(self, outputs):
@@ -62,7 +50,133 @@ class HomographyModel(pl.LightningModule):
         return {"avg_val_loss": avg_loss, "log": logs}
 
 
-class Net(nn.Module):
+class SupervisedNet(nn.Module):
+    def __init__(self, OutputSize=8):
+        """
+        Inputs:
+        OutputSize - Size of the Output
+        """
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=2, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+        )
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+        )
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=128 * 16 * 16, out_features=1024),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=1024, out_features=OutputSize),
+        )
+        self.features = nn.Sequential(
+            self.conv1,
+            self.conv2,
+            self.conv3,
+            self.conv4,
+            self.conv5,
+            self.conv6,
+            self.conv7,
+            self.conv8,
+        )
+
+    def forward(self, xa, xb):
+        """
+        Input:
+        xa is a MiniBatch of the image a
+        xb is a MiniBatch of the image b
+        Outputs:
+        out - output of the network
+        """
+        x = torch.cat([xa, xb], dim=1)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        out = self.fc(x)
+        return out
+
+
+class UnsupervisedHomographyModel(pl.LightningModule):
+    def __init__(self, hparams):
+        super(UnsupervisedHomographyModel, self).__init__()
+        self.hparams = hparams
+        self.model = UnsupervisedNet()
+
+    def forward(self, a, b):
+        return self.model(a, b)
+
+    def training_step(self, batch, batch_idx):
+        img_a, patch_a, patch_b, corners, gt = batch
+        delta = self.model(patch_a, patch_b)
+        loss = self.LossFn(delta, img_a, patch_b, corners)
+        logs = {"loss": loss}
+        return {"loss": loss, "log": logs}
+
+    def validation_step(self, batch, batch_idx):
+        img_a, patch_a, patch_b, corners, gt = batch
+        delta = self.model(patch_a, patch_b)
+        loss = self.LossFn(delta, img_a, patch_b, corners)
+        return {"val_loss": loss}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        logs = {"val_loss": avg_loss}
+        return {"avg_val_loss": avg_loss, "log": logs}
+
+    def LossFn(delta, patch_a, patch_b, corners):
+        B = delta.shape[0]
+        pred_corners = corners + delta.view(B, 4, 2)
+        H = kornia.geometry.get_perspective_transform(
+            corners.float(), pred_corners.float()
+        )
+        warped_a = kornia.geometry.warp_perspective(
+            patch_a,
+            H,
+            dsize=(patch_b.shape[-2], patch_b.shape[-1]),
+            padding_mode="border",
+        )
+        loss = F.l1_loss(warped_a, patch_b)
+        return loss
+
+
+class UnsupervisedNet(nn.Module):
+
     def __init__(self, InputSize, OutputSize):
         """
         Inputs:
@@ -126,4 +240,4 @@ class Net(nn.Module):
         #############################
         # Fill your network structure of choice here!
         #############################
-        return out
+        pass
