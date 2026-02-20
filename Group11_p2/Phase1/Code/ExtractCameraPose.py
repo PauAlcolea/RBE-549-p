c204de111bbf4ec2d8b3674085eab80164b528a4
Benjamin Orr <benorr3@gmail.com>
2026-02-20 12:05:33 -0500

    minor formatting adjustments

--- Group11_p2/Phase1/Code/ExtractCameraPose.py
+++ Group11_p2/Phase1/Code/ExtractCameraPose.py
@@ -1,17 +1,18 @@
 import numpy as np
 import sys
 
+
 def extractCameraPose(E: np.ndarray, K: np.ndarray) -> np.ndarray:
     """
-    Docstring for extractCameraPose, this function will extract the four different possible camera poses out of the Essential Matrix E
-    
+    This function will extract the four different possible camera poses out of the Essential Matrix E
+
     :param E: Essential matrix E
     :param K: Camera intrinsic Matrix
     :return: 4 possible poses that will get narrowed down with the triangulation
     """
-    
+
     # SVD from essential matrix
-    U, D, Vt = np.linalg.svd(E)
+    U, _, Vt = np.linalg.svd(E)
 
     S1 = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
     S2 = S1.T
@@ -23,7 +24,7 @@ def extractCameraPose(E: np.ndarray, K: np.ndarray) -> np.ndarray:
 
     # Two possible translations because epipole will lie on plane (left nullspace is in second image T.T @ E = 0)
     # T1 = u3
-    T1 = U[:,2]
+    T1 = U[:, 2]
     T2 = -T1
     T = (T1, T2)
 
@@ -32,23 +33,11 @@ def extractCameraPose(E: np.ndarray, K: np.ndarray) -> np.ndarray:
     P = []
     for t in T:
         for r in R:
-            # Check determinant and correct camera pose if it's -1. (is it supposed to be equal to 1??)
+            # Check determinant and correct camera pose if it's -1.
             if np.linalg.det(r) < 0:
                 t = -t
                 r = -r
             pose = K @ r @ np.column_stack((np.identity(3), t))
             P.append(pose)
 
-
     return np.array(P)
-
-# For testing purposes:
-# exampleE = [[1, 2, 0], 
-#             [3, 4, 5], 
-#             [6, 7, 8]]
-
-# K = [[531.122155322710, 0, 407.192550839899],
-#      [0, 531.541737503901, 313.308715048366],
-#      [0, 0, 1]]
-
-# print(extractCameraPose(exampleE, K))
