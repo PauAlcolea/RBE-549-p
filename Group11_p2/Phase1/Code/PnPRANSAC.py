63fd3f480877e837832b36682da069051a247384
PauAlcolea <pau@alcolea.eu>
2026-02-24 09:30:46 -0500

    remove synthetic tests

--- Group11_p2/Phase1/Code/PnPRANSAC.py
+++ Group11_p2/Phase1/Code/PnPRANSAC.py
@@ -9,7 +9,6 @@ def pnpRANSAC(xs, Xs, K):
     :param Xs are all of the 3D points that the 2D points are the projection of
     :param K the camera internal matrix
     """
-
     n = 0           # maximum inlier count so far, used to select the best set
     M = 1000        # How many iterations will this run for?
     N = 6           # How many inliers are going to be passed to the Linear PnP
@@ -49,70 +48,7 @@ def pnpRANSAC(xs, Xs, K):
             most_inliers_xs = inliers_xs      # keep track of the set of points that has the most inliers, these will be fed back to pnp for final R, T
             most_inliers_Xs = inliers_Xs
 
-    
-    final_R, final_T = linearPnP(xs=np.array(most_inliers_xs), Xs=np.array(most_inliers_Xs), K=K)
     # now pass that set of most inliers back to the linear pnp to get the real pose
+    final_R, final_T = linearPnP(xs=np.array(most_inliers_xs), Xs=np.array(most_inliers_Xs), K=K)
 
     return final_R, final_T
-
-
-def test_pnp_synthetic(num_points=100, outlier_ratio=0.2):
-    np.random.seed(1)
-
-    # --- Ground truth rotation ---
-    theta = np.radians(15)
-    R_gt = np.array([
-        [np.cos(theta), 0, np.sin(theta)],
-        [0, 1, 0],
-        [-np.sin(theta), 0, np.cos(theta)]
-    ])
-
-    # --- Ground truth translation ---
-    C_gt = np.array([0.5, -0.2, -4.0])
-    t_gt = -R_gt @ C_gt
-
-    # --- Camera intrinsics ---
-    K = np.array([
-        [800, 0, 320],
-        [0, 800, 240],
-        [0, 0, 1]
-    ], dtype=float)
-
-    # --- Generate 3D points ---
-    Xs = np.random.uniform(-2, 2, (num_points, 3))
-
-    # --- Projection matrix ---
-    P_gt = K @ np.hstack([R_gt, t_gt.reshape(3,1)])
-
-    xs = np.zeros((num_points, 2))
-
-    for i in range(num_points):
-        X_h = np.append(Xs[i], 1)
-        x_h = P_gt @ X_h
-        x_h /= x_h[2]
-        xs[i] = x_h[:2]
-   
-    R_est, T_est = pnpRANSAC(xs, Xs, K)
-
-    
-    
-    # Check rotation orthogonality
-    print("det(R):", np.linalg.det(R_est))  # Should be ~1.0
-    print("R R^T:\n", R_est @ R_est.T)       # Should be ~identity
-
-    # Check reprojection error
-    t_est = T_est
-    P_est = K @ np.hstack([R_est, t_est.reshape(3,1)])
-    errors = []
-    for x, X in zip(xs, Xs):
-        X_h = np.append(X, 1)
-        x_proj = P_est @ X_h
-        x_proj /= x_proj[2]
-        err = np.linalg.norm(x_proj[:2] - x)
-        errors.append(err)
-    print("Mean reprojection error:", np.mean(errors))  # Should be ~0.0
-
-    
-    return
-
-test_pnp_synthetic()
