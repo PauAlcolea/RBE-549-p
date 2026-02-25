92b7d83929991bf314de81e7dd8462bd788f1c0a
PauAlcolea <pau@alcolea.eu>
2026-02-25 11:18:40 -0500

    non-linear pnp draft

--- Group11_p2/Phase1/Code/NonlinearPnP.py
+++ Group11_p2/Phase1/Code/NonlinearPnP.py
@@ -1,35 +1,46 @@
 import numpy as np
 from LinearPnP import linearPnP
+from PnPRANSAC import pnpRANSAC
 from scipy.optimize import least_squares
+from scipy.spatial.transform import Rotation as Rot 
 
 
-def reprojection_error(guess: tuple[np.ndarray, np.ndarray], K: np.ndarray, xs:np.ndarray, Xs:np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+def reprojection_error(params: np.ndarray, K: np.ndarray, xs:np.ndarray, Xs:np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     """
-    :return R and T, this will be optimiezd by the least squares
+    :return q and T, this will be optimiezd by the least squares
     """
-    loss_list = []
 
-    R0, T0 = guess
-    P = K @ np.hstack([R0, T0.reshape(3,1)])
+    # extract all of the parameters
+    T = params[0:3]
+    q = params[3:7]
+
+    # normalization of quaternion
+    q = q / np.linalg.norm(q)
+
+    # turn quaternion back into a rotation matrix
+    rot = Rot.from_quat(q)
+    R = rot.as_matrix()
+
+    P = K @ np.hstack([R, T.reshape(3,1)])
     P_1 = P[0, :]
     P_2 = P[1, :]
     P_3 = P[2, :]
 
+    loss_list = []
     for (u, v), X in zip(xs, Xs):
 
         X_tilde = np.append(X, 1)
 
         e1 = (u - (P_1 @ X_tilde) / (P_3 @ X_tilde))
         e2 = v - (P_2 @ X_tilde) / (P_3 @ X_tilde)
-        error = e1**2 + e2**2
         
-        loss_list.append(error)
+        loss_list.append([e1, e2])
 
     residuals = np.concatenate(loss_list)
     return residuals
 
 
-def nonlinearPnP(xs: np.ndarray, Xs: np.ndarray, K: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+def nonlinearPnP(xs: np.ndarray, Xs: np.ndarray, K: np.ndarray, R0, T0) -> tuple[np.ndarray, np.ndarray]:
     """
     this function is used to calculate the pose of the camera for the added viewpoint in a non-linear manner
     This involves proper optimization by reducing the reprojection error as opposed to algebraic error minimized by the linear pnp
@@ -39,12 +50,25 @@ def nonlinearPnP(xs: np.ndarray, Xs: np.ndarray, K: np.ndarray) -> tuple[np.ndar
         each point is an np.ndarray (x, y)
     :param X is a list of 6 3D points that correspond to x
     :K camera intrinsic matrix (3,3)
-    """
+    """  
+    # make a function that takes rotation and makes it quaternion and vice versa
+    rot = Rot.from_matrix(R0)
+    q0 = rot.as_quat()
 
-    initial_guess = linearPnP(xs, Xs, K)
+    initial_parameters = np.hstack((T0, q0))
 
-    print("STARTING NON-LINEAR OPTIMIZATION: ")
-    result = least_squares(reprojection_error, initial_guess, args=(K, xs, Xs), method='lm')
-    print("OPTIMIZATION COMPLETE")
+    print("Starting Non-Linear Optimization: ")
+    result = least_squares(reprojection_error, 
+                           initial_parameters, 
+                           args=(K, xs, Xs), 
+                           method='lm')
+    print("Optimization Complete")
 
-    pass
+    optimized_params = result.x
+    T_opt = optimized_params[0:3]
+    q_opt = optimized_params[3:7]
+
+    q_opt = q_opt / np.linalg.norm(q_opt)
+    R_opt = Rot.from_quat(q_opt).as_matrix()
+
+    return R_opt, T_opt
