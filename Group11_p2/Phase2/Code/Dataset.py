ed9ef02d59fd6dd19fbd0ab2971a8408d4679b1f
Benjamin Orr <benorr3@gmail.com>
2026-03-03 19:48:20 -0500

    initial work on class structure

--- Group11_p2/Phase2/Code/Dataset.py
+++ Group11_p2/Phase2/Code/Dataset.py
@@ -3,5 +3,26 @@ import random
 import numpy as np
 import torch
 
+
 class NeRFDataset:
-    pass
+    """
+    dataset class for NeRF training (bypassing PyTorch Dataset/DataLoader)
+    """
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def get_sample(self, idx):
+        pass
+
+    def get_random_sample(self):
+        pass
+
+    def get_batch(self, batch_size):
+        pass
+
+    def get_batch_from_index(self, start_idx, batch_size):
+        pass
