c5b1b8082b07aaf4e975f48d2e7852c6db5849ad
Benjamin Orr <benorr3@gmail.com>
2026-03-03 22:07:23 -0500

    filled out more functions

--- Group11_p2/Phase2/Code/Dataset.py
+++ Group11_p2/Phase2/Code/Dataset.py
@@ -16,6 +16,8 @@ class NeRFDataset:
         self.data_dir = Path(data_dir)
         self.json_data = self._load_json()
         self.images = self._load_images()
+        self.h = self.images[0].shape[0]
+        self.w = self.images[0].shape[1]
         self.K = self._compute_intrinsics()
         self.poses = self._get_poses()
         self.ray_directions = self._get_camera_ray_directions()
@@ -34,9 +36,10 @@ class NeRFDataset:
 
     def _compute_intrinsics(self):
         FOV_x = self.json_data["camera_angle_x"]
-        h, w = self.images[0].shape[:2]
-        f = 0.5 * w / np.tan(0.5 * FOV_x)
-        K = np.array([[f, 0, w / 2], [0, f, h / 2], [0, 0, 1]], dtype=np.float32)
+        f = 0.5 * self.w / np.tan(0.5 * FOV_x)
+        K = np.array(
+            [[f, 0, self.w / 2], [0, f, self.h / 2], [0, 0, 1]], dtype=np.float32
+        )
         return K
 
     def _get_poses(self):
@@ -49,9 +52,8 @@ class NeRFDataset:
         """
         for each pixel compute ray direction from camera center through that pixel
         """
-        h, w = self.images[0].shape[:2]
         # create grid of homogeneous pixel coordinates
-        i, j = np.meshgrid(np.arange(w), np.arange(h), indexing="xy")
+        i, j = np.meshgrid(np.arange(self.w), np.arange(self.h), indexing="xy")
         pixel_coords = np.stack([i, j, np.ones_like(i)], axis=-1)
         # compute ray directions in camera space
         ray_directions = pixel_coords @ np.linalg.inv(self.K).T
@@ -69,19 +71,37 @@ class NeRFDataset:
         R, t = pose[:3, :3], pose[:3, 3]
         ray_directions_world = self.ray_directions @ R.T
         ray_origins_world = np.broadcast_to(t, ray_directions_world.shape)
-        return ray_origins_world, ray_directions_world
+        return ray_origins_world.reshape(-1, 3), ray_directions_world.reshape(-1, 3)
 
     def __len__(self):
-        return len(self.images)
+        # total number of rays in all images
+        return len(self.images) * self.h * self.w
+
+    def _get_image(self, idx):
+        rgb = self.images[idx]
+        return rgb.reshape(-1, 3)
 
     def get_sample(self, idx):
-        pass
+        """
+        get a single ray sample (origin, direction) and its corresponding RGB color
+        """
+        img_idx = idx // (self.h * self.w)
+        pixel_idx = idx % (self.h * self.w)
+        rgb = self._get_image(img_idx)
+        ray_origins, ray_directions = self._get_rays_for_image(img_idx)
+        return ray_origins[pixel_idx], ray_directions[pixel_idx], rgb[pixel_idx]
 
     def get_random_sample(self):
-        pass
+        idx = random.randint(0, len(self.images) - 1)
+        return self.get_sample(idx)
 
     def get_batch(self, batch_size):
-        pass
+        for _ in range(batch_size):
+            yield self.get_random_sample()
 
     def get_batch_from_index(self, start_idx, batch_size):
-        pass
+        end_idx = min(start_idx + batch_size, len(self.images) * self.h * self.w)
+        batch = []
+        for idx in range(start_idx, end_idx):
+            batch.append(self.get_sample(idx))
+        return batch
