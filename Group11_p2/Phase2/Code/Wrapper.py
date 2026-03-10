1ede000a1a939f54a335baeccb2c0d420581806a
Benjamin Orr <benorr3@gmail.com>
2026-03-10 15:46:00 -0400

    swapped function placement

--- Group11_p2/Phase2/Code/Wrapper.py
+++ Group11_p2/Phase2/Code/Wrapper.py
@@ -17,26 +17,6 @@ def render(model, rays_origin, rays_direction, args):
     pass
 
 
-def main(args):
-    device = (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps" if torch.backends.mps.is_available() else "cpu"
-    )
-
-    top_data_dir = Path(__file__).parent.parent / "Data" / "nerf_synthetic"
-    dataset_dir = top_data_dir / args.dataset
-
-    if args.test:
-        raise NotImplementedError("TODO: implement test mode")
-    else:
-        train(
-            train_data_dir=dataset_dir / "train",
-            val_data_dir=dataset_dir / "val",
-            device=device,
-        )
-
-
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -56,6 +36,26 @@ def parseArgs():
 
     args = parser.parse_args()
     return args
+
+
+def main(args):
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+
+    top_data_dir = Path(__file__).parent.parent / "Data" / "nerf_synthetic"
+    dataset_dir = top_data_dir / args.dataset
+
+    if args.test:
+        raise NotImplementedError("TODO: implement test mode")
+    else:
+        train(
+            train_data_dir=dataset_dir / "train",
+            val_data_dir=dataset_dir / "val",
+            device=device,
+        )
 
 
 if __name__ == "__main__":
